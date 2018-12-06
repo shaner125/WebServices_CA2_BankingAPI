1d5eb8f18a025dcfab3126524049e9e3ce39d19f
James-Curran <curran.p.james@gmail.com>
2018-12-06 11:24:17 +0000

    Build

---
+++ front-end/build/precache-manifest.f90f9bacf74da2f2be4879ad0f03b53e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e7690d2103274084195eff4b1c8afda",
+    "url": "./static/media/withdraw.5e7690d2.svg"
+  },
+  {
+    "revision": "74e9eaaac27039b4a4e3871ee68b4a17",
+    "url": "./static/media/transfer.74e9eaaa.svg"
+  },
+  {
+    "revision": "d5c81b885787541a83cb3c09162cbc7f",
+    "url": "./static/media/lodgenent.d5c81b88.svg"
+  },
+  {
+    "revision": "9882730f0ffbd43089d2600dbb77bbbe",
+    "url": "./static/media/homeButton.9882730f.svg"
+  },
+  {
+    "revision": "d3046b6307e2077eddc75336bae44709",
+    "url": "./static/media/add.d3046b63.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ed1670d1cfa0ba9f53bf",
+    "url": "./static/js/main.ed1670d1.chunk.js"
+  },
+  {
+    "revision": "2ebf1f91e972a167b175",
+    "url": "./static/js/1.2ebf1f91.chunk.js"
+  },
+  {
+    "revision": "ed1670d1cfa0ba9f53bf",
+    "url": "./static/css/main.51d0b84f.chunk.css"
+  },
+  {
+    "revision": "878c7af9f84c0b91facb43a17ef5a3f5",
+    "url": "./index.html"
+  }
+];
