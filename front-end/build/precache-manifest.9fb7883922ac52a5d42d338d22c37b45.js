8ad02f41032a22fd87fdcc506921b6627d46d913
James-Curran <curran.p.james@gmail.com>
2018-12-06 11:50:44 +0000

    Build

---
+++ front-end/build/precache-manifest.9fb7883922ac52a5d42d338d22c37b45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "522c4cd550c874fa0918dd8813a15e81",
+    "url": "./static/media/withdraw.522c4cd5.svg"
+  },
+  {
+    "revision": "c985ee2ed200b392a6892f2db8645aba",
+    "url": "./static/media/transfer.c985ee2e.svg"
+  },
+  {
+    "revision": "c34dd796573473f759cd07706e94b595",
+    "url": "./static/media/lodgenent.c34dd796.svg"
+  },
+  {
+    "revision": "6782fddbcfc156a39b218983d47046a5",
+    "url": "./static/media/homeButton.6782fddb.svg"
+  },
+  {
+    "revision": "5de58c43ce3748ebc2e73128113f0587",
+    "url": "./static/media/add.5de58c43.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8459e613eab40cad0a3a",
+    "url": "./static/js/main.8459e613.chunk.js"
+  },
+  {
+    "revision": "2ebf1f91e972a167b175",
+    "url": "./static/js/1.2ebf1f91.chunk.js"
+  },
+  {
+    "revision": "8459e613eab40cad0a3a",
+    "url": "./static/css/main.51d0b84f.chunk.css"
+  },
+  {
+    "revision": "c4a8544a032d56b7bf0287bcbd4c60e6",
+    "url": "./index.html"
+  }
+];
--- front-end/build/service-worker.js
+++ front-end/build/service-worker.js
@@ -3,18 +3,18 @@
  *
  * You'll need to register this file in your web app and you should
  * disable HTTP caching for this file too.
- * See http://goo.gl/nhQhGp
+ * See https://goo.gl/nhQhGp
  *
  * The rest of the code is auto-generated. Please don't update this file
  * directly; instead, make changes to your Workbox build configuration
  * and re-run your build process.
- * See http://goo.gl/2aRDsh
+ * See https://goo.gl/2aRDsh
  */
 
-importScripts("http://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e13d9daf8de79b83edbb37319b702cbd.js"
+  "./precache-manifest.9fb7883922ac52a5d42d338d22c37b45.js"
 );
 
 workbox.clientsClaim();
@@ -22,13 +22,13 @@ workbox.clientsClaim();
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
- * See http://goo.gl/S9QRab
+ * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
-
+  
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
