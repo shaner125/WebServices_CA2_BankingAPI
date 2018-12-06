7f731aad562008a17fc759e58c516bdc880b608d
James-Curran <curran.p.james@gmail.com>
2018-12-06 11:42:07 +0000

    Build

--- front-end/build/service-worker.js
+++ front-end/build/service-worker.js
@@ -3,15 +3,15 @@
  *
  * You'll need to register this file in your web app and you should
  * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
+ * See http://goo.gl/nhQhGp
  *
  * The rest of the code is auto-generated. Please don't update this file
  * directly; instead, make changes to your Workbox build configuration
  * and re-run your build process.
- * See https://goo.gl/2aRDsh
+ * See http://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("http://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
   "./precache-manifest.e13d9daf8de79b83edbb37319b702cbd.js"
@@ -22,13 +22,13 @@ workbox.clientsClaim();
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
+ * See http://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
-  
+
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
